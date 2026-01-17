1c2fbba1ae09d62f6c876fda284e589beaf11e84
SatyamRaj2007 <25BCS13064@cuchd.in>
2026-01-17 19:57:52 +0530

    crud

--- 65_CRUD_vector.cpp
+++ 65_CRUD_vector.cpp
@@ -1,22 +1,41 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-int main(){
 
+int main() {
     int n;
-    cout<<"enter size of vector";
-    cin>>n;
-    vector<int>vec(n);
-    for(int i=0;i<n;i++){
-        cin>>vec[i];
-    }
-    for(int i=0;i<n;i++){
-        cout<<vec[i]<<" ";
+    cout << "enter size of vector: ";
+    cin >> n;
+
+    vector<int> vec(n);
+
+    cout << "enter element of vector" << endl;
+    for(int i = 0; i < n; i++) {
+        cin >> vec[i];
     }
 
-    int choice=0;
-    while(choice!=0)
-    cout<<"choice:";
-     
+    cout << "\nelement of vector:\n";
+    for(int i = 0; i < vec.size(); i++) {
+        cout << vec[i] << " ";
+    }
+
+    cout << endl;
+
+    vec.push_back(10);
+    vec.push_back(20);
+    vec.push_back(30);
+
+    cout << "\nafter adding element in vector:\n";
+    for(int i = 0; i < vec.size(); i++) {
+        cout << vec[i] << " ";
+    }
+
+    vec.pop_back();
+
+    cout << "\nafter deleting element in vector:\n";
+    for(int i = 0; i < vec.size(); i++) {
+        cout << vec[i] << " ";
+    }
+
     return 0;
 }
