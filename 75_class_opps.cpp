7f122c790ff6e109dfd9b602440c7cf0f708da9f
SatyamRaj2007 <25BCS13064@cuchd.in>
2026-01-31 11:50:54 +0530

    class and oops

---
+++ 75_class_opps.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+class student{
+    public:
+    int a,b;
+};
+
+int main(){
+    student o;
+    o.a=2;
+    o.b=10;
+    cout<<o.a<<" "<<o.b<<endl;
+    return 0;
+}
