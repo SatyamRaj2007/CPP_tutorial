2b3b32c92f702b102de2ae6ccc53f69d3eb2ee55
SatyamRaj2007 <25BCS13064@cuchd.in>
2026-07-26 11:15:14 +0530

    subarray

---
+++ 123_print_subarray.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int sum;
+    cin>>sum;
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            int s=0;
+            for(int k=i;k<=j;k++){
+                s+=arr[k];
+            }
+            if(s==sum){
+                cout<<i<<" "<<j<<endl;
+            }
+        }
+    }
+}
